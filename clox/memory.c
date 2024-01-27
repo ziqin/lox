eb56cd45ea231a7b48de33ff4fc318f90d2bfe81
Ziqin Wang <logicalclock@outlook.com>
2024-01-28 03:15:25 +0800

    chore(clox): remove repetitive comment

--- clox/memory.c
+++ clox/memory.c
@@ -2,12 +2,6 @@
 
 #include "memory.h"
 
-// | oldSize  | newSize              | Operation                   |
-// | -------- | -------------------- | --------------------------- |
-// | 0        | Non-zero             | Allocate new block.         |
-// | Non-zero | 0                    | Free allocation.            |
-// | Non-zero | Smaller than oldSize | Shrink existing allocation. |
-// | Non-zero | Larger than oldSize  | Grow existing allocation.   |
 void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
   if (newSize == 0) {
     free(pointer);
