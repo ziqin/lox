cc276f44c2f15354925cbbb921ac26ea8a9d4a84
Ziqin Wang <logicalclock@outlook.com>
2024-05-03 16:16:59 +0800

    refactor(clox): optimize Value using NaN boxing
    Closes #19

--- clox/common.h
+++ clox/common.h
@@ -11,11 +11,13 @@
 #define UNUSED(a) UNUSED_ ## a
 #endif
 
-#define DEBUG_PRINT_CODE
-#define DEBUG_TRACE_EXECUTION
+#define NAN_BOXING
 
-#define DEBUG_STRESS_GC
-#define DEBUG_LOG_GC
+//#define DEBUG_PRINT_CODE
+//#define DEBUG_TRACE_EXECUTION
+
+//#define DEBUG_STRESS_GC
+//#define DEBUG_LOG_GC
 
 #define UINT8_COUNT (UINT8_MAX + 1)
 
--- clox/value.h
+++ clox/value.h
@@ -6,6 +6,46 @@
 typedef struct Obj Obj;
 typedef struct ObjString ObjString;
 
+#ifdef NAN_BOXING
+
+#define SIGN_BIT  ((uint64_t)0x8000000000000000)
+// All of the exponent bits, plus the quiet NaN bit, plus one extra to dodge
+// that Intel value.
+#define QNAN      ((uint64_t)0x7ffc000000000000)
+
+#define TAG_NIL   1 // 01.
+#define TAG_FALSE 2 // 10.
+#define TAG_TRUE  3 // 11.
+
+typedef uint64_t Value;
+
+#define IS_BOOL(value)   (((value) | 1) == TRUE_VAL)
+#define IS_NIL(value)    ((value) == NIL_VAL)
+#define IS_NUMBER(value) (((value) & QNAN) != QNAN)
+#define IS_OBJ(value)    (((value) & (QNAN | SIGN_BIT)) == (QNAN | SIGN_BIT))
+
+#define AS_BOOL(value)   ((value) == TRUE_VAL)
+#define AS_NUMBER(value) valueToNum(value)
+#define AS_OBJ(value)    ((Obj*)(uintptr_t)((value) & ~(SIGN_BIT | QNAN)))
+
+#define BOOL_VAL(b)     ((b) ? TRUE_VAL : FALSE_VAL)
+#define FALSE_VAL       ((Value)(uint64_t)(QNAN | TAG_FALSE))
+#define TRUE_VAL        ((Value)(uint64_t)(QNAN | TAG_TRUE))
+#define NIL_VAL         ((Value)(uint64_t)(QNAN | TAG_NIL))
+#define NUMBER_VAL(num) numToValue(num)
+// Most widely used chips today only ever use the low 48 bits for pointers.
+#define OBJ_VAL(obj)    (Value)(SIGN_BIT | QNAN | (uint64_t)(uintptr_t)(obj))
+
+static inline double valueToNum(Value value) {
+  return *(double*)&value;
+}
+
+static inline Value numToValue(double num) {
+  return *(Value*)&num;
+}
+
+#else // NAN_BOXING
+
 typedef enum {
   VAL_BOOL,
   VAL_NIL,
@@ -35,6 +75,8 @@ typedef struct {
 #define NIL_VAL           ((Value){VAL_NIL, {.number = 0}})
 #define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
 #define OBJ_VAL(object)   ((Value){VAL_OBJ, {.obj = (Obj*)object}})
+
+#endif // NAN_BOXING
 
 // Constant pool
 typedef struct {
